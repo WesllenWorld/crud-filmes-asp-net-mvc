ff68be5100bb9c8d07e2f5e425ad14e23670c215
WesllenWorld <wesllen.2020111052@unicap.br>
2025-05-19 11:34:55 -0300

    Adicionar arquivos de projeto.

---
+++ MVCFilmes2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCFilmes2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
