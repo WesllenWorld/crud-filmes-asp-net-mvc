4f5b9346174db3de537d715fa40bc07a10394393
WesllenWorld <wesllen.2020111052@unicap.br>
2025-05-21 22:09:44 -0300

    Validações

--- MVCFilmes2025/Migrations/20250522010924_acentuacoes no genero.Designer.cs
+++ MVCFilmes2025/Migrations/20250522010924_acentuacoes no genero.Designer.cs
@@ -4,6 +4,7 @@ using MVCFilmes2025.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCFilmes2025.Migrations
 {
     [DbContext(typeof(MVCFilmes2025Context))]
-    partial class MVCFilmes2025ContextModelSnapshot : ModelSnapshot
+    [Migration("20250522010924_acentuacoes no genero")]
+    partial class acentuacoesnogenero
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MVCFilmes2025/Models/Filme.cs
+++ MVCFilmes2025/Models/Filme.cs
@@ -7,16 +7,25 @@ namespace MVCFilmes2025.Models
         public int ID { get; set; }
 
         [Display(Name = "Título")]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
         public string Titulo { get; set; } = string.Empty;
 
         [Display(Name = "Data de lançamento")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
         [DataType(DataType.Date)]
         public DateTime DataLancamento { get; set; }
 
         [Display(Name = "Gênero")]
+        [Required(ErrorMessage = "Este campo é obrigatório."),
+            StringLength(20),
+            RegularExpression(@"^[A-ZÀ-Ý]+[a-zA-ZÀ-ÿ\s]*$", ErrorMessage ="Dados inválidos.")]
+        
         public string Genero { get; set; } = string.Empty;
 
         [Display(Name = "Avaliação")]
+        [Range(1,10, ErrorMessage ="Somente valores de 1 a 10.")]
+        //[Required(ErrorMessage = "Este campo é obrigatório.")]
         public int Avaliacao { get; set; } = 0;
     }
 }
